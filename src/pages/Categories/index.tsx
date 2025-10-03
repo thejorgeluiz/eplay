7f6342694c5ee6b0c8a822001e1e69adf6c8ca2e
jorgeluiz <thejljorgeluiz@gmail.com>
2025-10-03 20:58:27 -0300

    criação de algumas páginas, pastas e arquivo route.tsx

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,104 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../../src/assets/images/resident.png'
+import diablo from '../../../src/assets/images/diablo.png'
+import zelda from '../../../src/assets/images/zelda.png'
+import starWars from '../../../src/assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Lançado originalmente em 2005, Resident Evil 4 é um dos jogos mais marcantes da franquia de survival horror da Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Lançado originalmente em 2005, Resident Evil 4 é um dos jogos mais marcantes da franquia de survival horror da Capcom.',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Lançado originalmente em 2005, Resident Evil 4 é um dos jogos mais marcantes da franquia de survival horror da Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Lançado originalmente em 2005, Resident Evil 4 é um dos jogos mais marcantes da franquia de survival horror da Capcom.',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$250,00'],
+    image: resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo 4',
+    system: 'windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Zelda',
+    system: 'windows',
+    infos: ['17/05'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Star Wars',
+    system: 'windows',
+    infos: ['17/05'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Resident Evil 4',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: resident
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
