7bc600ae0857150ffc0e1edf9270a0c236d84015
jorgeluiz <thejljorgeluiz@gmail.com>
2025-10-10 22:04:54 -0300

    pasta Gallery

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,23 @@
+import Section from '../Section'
+import { Item, Items } from './styles'
+import zelda from '../../assets/images/banner-homem-aranha.png'
+
+const Gallery = () => (
+  <Section title="Galeria" background="black">
+    <Items>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+    </Items>
+  </Section>
+)
+export default Gallery
